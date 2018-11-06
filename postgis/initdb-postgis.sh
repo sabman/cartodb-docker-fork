e049d03ea799b867395de46f09e517bf4206f382
Mina Nagy Zaki <mnzaki@gmail.com>
2018-11-06 14:42:34 +0200

    postgis: add crankshaft and create extensions

--- postgis/initdb-postgis.sh
+++ postgis/initdb-postgis.sh
@@ -15,5 +15,16 @@ createuser tileuser --no-createrole --no-createdb --no-superuser -U $PGUSER
 # clones this template database
 echo "Creating database 'template_postgis'..."
 createdb -T template0 -O postgres -U $PGUSER -E UTF8 template_postgis
-echo "Creating extensions 'postgis' and 'postgis_topology' on database 'template_postgis'..."
-psql -U $PGUSER template_postgis -c 'CREATE EXTENSION postgis;CREATE EXTENSION postgis_topology;'
+createlang -d template_postgis plpgsql;
+psql -d postgres -c "UPDATE pg_database SET datistemplate='true' \
+  WHERE datname='template_postgis'"
+psql -c "CREATE EXTENSION plpythonu;"
+
+echo "Creating extensions postgis, postgis_topology, plpythonu, crankshaft, plproxy"
+psql -U $PGUSER template_postgis -c \
+  "CREATE EXTENSION postgis;CREATE EXTENSION postgis_topology;\
+   GRANT ALL ON geometry_columns TO PUBLIC;\
+   GRANT ALL ON spatial_ref_sys TO PUBLIC;\
+   CREATE EXTENSION plpythonu;\
+   CREATE EXTENSION crankshaft;\
+   CREATE EXTENSION plproxy;"
