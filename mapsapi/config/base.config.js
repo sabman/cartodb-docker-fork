012c78130d912502e048e7fb558f560563b57282
Mina Nagy Zaki <mnzaki@gmail.com>
2018-11-06 11:32:20 +0200

    mapsapi: PUBLIC_URL settings

--- mapsapi/config/base.config.js
+++ mapsapi/config/base.config.js
@@ -1,3 +1,11 @@
+const PUBLIC_HOST = process.env.PUBLIC_HOST || 'localhost';
+const PUBLIC_PORT = process.env.PUBLIC_PORT || '80';
+const PUBLIC_URL = (
+  PUBLIC_PORT == '80'
+  ? PUBLIC_HOST
+  : `${PUBLIC_HOST}:${PUBLIC_PORT}`
+);
+
 module.exports = {
   host: '0.0.0.0',
   user_from_host: '^([^\\.]+)\\.',
@@ -7,4 +15,8 @@ module.exports = {
   redis: {
     host: 'cartodb-redis',
   },
+  resources_url_templates: {
+    http: 'http://' + PUBLIC_URL + '/{{=it.user}}/api/v1/map',
+    https: 'https://' + PUBLIC_URL + '/{{=it.user}}/api/v1/map'
+  }
 };
