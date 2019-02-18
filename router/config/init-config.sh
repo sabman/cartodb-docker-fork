eeee90dac6c562c7a2ca2de298ce2b5a4ea88958
Shoaib Burq <saburq@gmail.com>
2019-02-18 12:47:37 +0100

    add build ARGs to router for setting CERT_PATH and HOST

--- router/config/init-config.sh
+++ router/config/init-config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 /bin/sed \
 -e "s~\${PUBLIC_HOST}~${PUBLIC_HOST}~" \
 -e "s~\${CERTS_PATH}~${CERTS_PATH}~"  \
 < /etc/nginx/conf.d/default.conf.tmpl \
 > /etc/nginx/conf.d/default.conf
+
