b18c15c223f3997b19bed35758cde5c6a78d0dd7
Shoaib Burq <saburq@gmail.com>
2019-02-19 05:13:30 +0100

    use the CERTS_PATH for the mount location

--- router/config/init-config.sh
+++ router/config/init-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 /bin/sed \
 -e "s~\${PUBLIC_HOST}~${PUBLIC_HOST}~" \
@@ -6,3 +7,18 @@
 < /etc/nginx/conf.d/default.conf.tmpl \
 > /etc/nginx/conf.d/default.conf
 
+## This is an attempt at creating a self signed certificate 
+# =========================================================
+
+## Generate dhparam:
+# openssl dhparam -out ${CERTS_PATH}/dhparam-2048.pem 2048
+
+## Cenerate key and cert files:
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout privkey.pem -out fullchain.pem -subj "/C=DE/ST=Berlin/L=Berlin/O=OrgName/OU=IT Department/CN=${PUBLIC_HOST}"
+
+## Create location for the CERTS based on public hostname
+# mkdir -p "${CERTS_PATH}/${PUBLIC_HOST}"
+# mv privkey.pem ${CERTS_PATH}/${PUBLIC_HOST}/
+# mv fullchain.pem ${CERTS_PATH}/${PUBLIC_HOST}/
+
+# nginx -g  "daemon off;"
