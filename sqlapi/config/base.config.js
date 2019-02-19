2251e7941c2f73684a1029ec4aed71386956ded0
Shoaib Burq <saburq@gmail.com>
2019-02-19 06:58:41 +0100

    update db_port since it's using the pgbouncer setup

--- sqlapi/config/base.config.js
+++ sqlapi/config/base.config.js
@@ -4,6 +4,8 @@ module.exports = {
   user_from_host: '^([^\\.]+)\\.',
   node_host: '0.0.0.0',
   db_host: 'cartodb-postgis',
+  // db_port: "6432", // if pgbouncer is configured
+  db_port: '5432',
   redis_host: 'cartodb-redis',
   // Send log messages to stdout.
   log_filename: undefined,
