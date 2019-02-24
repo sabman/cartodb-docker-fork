acf63a7bedbb9168a258b7d3e638d67733b4ab19
Shoaib Burq <saburq@gmail.com>
2019-02-24 17:35:24 +0500

    WIP: try to fix the named maps not showing; readme with open issues

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 - [x] Fix "Sorry, we can't find CARTO user 'admin'. Please check that you have entered the correct domain."
   > Related to https://github.com/CartoDB/node-cartodb-redis/blob/v0.3.0/lib/carto_metadata.js#L81 > https://github.com/CartoDB/CartoDB-SQL-API/issues/149 > `docker-compose exec cartodb-editor ./script/restore_redis`
-- [ ] Cannot connect to database
+- [x] Cannot connect to database
 - [ ] mapsapi check: `hostHeaderTemplate: "{{=it.username}}.localhost.lan"`
 - [ ] check port for app_config
+- [ ] `{"error":["PL/Proxy function cdb_dataservices_client._cdb_service_quota_info(2): [dataservices_db] PQconnectPoll: FATAL: role \"geocoder_api\" does not exist\n"]}`
+- [ ] Running analysis gives: `Unable to enqueue SQL API batch job`
+- When previewing the data in tables getting this error: `"Template 'tpl_077e94bf_8fa9_4684_96af_b8b58aa624b4' of user 'admin' not found"`
--- mapsapi/config/base.config.js
+++ mapsapi/config/base.config.js
@@ -8,21 +8,47 @@ const PUBLIC_URL = (
 
 module.exports = {
   host: '0.0.0.0',
-  user_from_host: '^([^\\.]+)\\.',
+  node_host: '0.0.0.0',
+  // user_from_host: '^([^\\.]+)\\.',
+  user_from_host: '\/user\/(.*)$',
   postgres: {
+    host: 'cartodb-postgis',
+  },
+  postgis: {
     host: 'cartodb-postgis',
   },
   redis: {
     host: 'cartodb-redis',
   },
   resources_url_templates: {
-    http: 'http://' + PUBLIC_URL + '/user/{{=it.user}}/api/v1/map',
-    https: 'https://' + PUBLIC_URL + '/user/{{=it.user}}/api/v1/map'
+    http: PUBLIC_URL + '/user/{{=it.user}}/api/v1/map',
+    https: PUBLIC_URL + '/user/{{=it.user}}/api/v1/map'
+  }
+  // Base URLs for the APIs
+  //
+  // See http://github.com/CartoDB/Windshaft-cartodb/wiki/Unified-Map-API
+  //
+  // Base url for the Templated Maps API
+  // "/api/v1/map/named" is the new API,
+  // "/tiles/template" is for compatibility with versions up to 1.6.x
+  ,base_url_templated: '(?:/api/v1/map/named|/user/:user/api/v1/map/named|/tiles/template)'
+  // Base url for the Detached Maps API
+  // "maps" is the the new API,
+  // "tiles/layergroup" is for compatibility with versions up to 1.6.x
+  ,base_url_detached: '(?:/api/v1/map|/user/:user/api/v1/map|/tiles/layergroup)'
+  ,serverMetadata: {
+    cdn_url: {
+      http: PUBLIC_URL + '/user',
+      https: PUBLIC_URL + '/user',
+    },
   },
   analysis: {
     batch: {
-      endpoint: "http://cartodb-router/api/v2/sql/job",
+      endpoint: "https://cartodb-router/api/v2/sql/job",
       hostHeaderTemplate: "{{=it.username}}.localhost.lan"
     }
   }
+  // , "varnish": { TODO: add varnish
+  //   "host": "localhost",
+  // }
 };
