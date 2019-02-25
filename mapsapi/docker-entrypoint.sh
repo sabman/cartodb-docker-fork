3001afde12fbd81e3f1c2dca505855785d1cbcd3
Shoaib Burq <saburq@gmail.com>
2019-02-26 04:49:04 +0500

    millstone

--- mapsapi/docker-entrypoint.sh
+++ mapsapi/docker-entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 # create the output directory for the config file
 mkdir -p /etc/cartodb
 
+# create cache directory
+mkdir -p /tmp/cdb-tiler/millstone
+
 echo "Writing the configuration file /etc/cartodb/config.js..."
 node config/environments/merge-modules -i $@ -o /etc/cartodb/config.js
 
