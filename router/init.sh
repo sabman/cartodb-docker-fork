a7fc86c2a2ec37c688d6f893d1ba511433301a05
Mina Nagy Zaki <mnzaki@gmail.com>
2019-05-16 19:50:11 +0200

    router: remove stream and deployment.d confs
    we don't need them, nginx config template is already presetup

--- router/init.sh
+++ router/init.sh
@@ -31,10 +31,6 @@ DOMAIN=$(echo $DOMAINS | cut -d ',' -f 1) SERVER=${server} envsubst < /etc/nginx
 for server in $(echo $DOMAINS | sed 's/,/ /g' )
 do
   DOMAIN=$(echo $DOMAINS | cut -d ',' -f 1) SERVER=${server} envsubst < /etc/nginx/server.conf.tmpl > /etc/nginx/site.d/${server}.conf
-  if [ ! -e /etc/nginx/deployment.d/${server}.conf.* ] && [ ! -e /etc/nginx/conf.d/${server}.conf.* ]
-  then
-    echo "return 502;" > /etc/nginx/deployment.d/${server}.conf.default
-  fi
 done
 NUMPROCS=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage_percpu | wc -w)
 sed -i "s/worker_processes\\s\\+1;/worker_processes ${NUMPROCS};/" /etc/nginx/nginx.conf
