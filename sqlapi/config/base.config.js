442b7ffdb8a7beb7d3578c3ab58c948c02234a07
Mina Nagy Zaki <mnzaki@gmail.com>
2019-05-31 18:18:41 +0200

    sqlapi: fix db port

--- sqlapi/config/base.config.js
+++ sqlapi/config/base.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   node_host: '0.0.0.0',
   db_host: 'cartodb-postgis',
+  db_port: 5432,
   redis_host: 'cartodb-redis',
   // Send log messages to stdout.
   log_filename: undefined,
